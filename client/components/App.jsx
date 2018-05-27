20abc67b7dc43838bc237f5381c9cbf6f32b3139
Kevin Jacobs <frevin8r@gmail.com>
2018-05-27 14:37:05 -0700

    webpack config, rendered react app

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <div>
+        The app has rendered
+      </div>
+    )
+  }
+}
