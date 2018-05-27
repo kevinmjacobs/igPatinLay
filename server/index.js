cf1cbebaf0ea5abb331fea5c5f5995807f106a5f
Kevin Jacobs <frevin8r@gmail.com>
2018-05-27 14:14:52 -0700

    init repo and build server

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const parser = require('body-parser');
+const helmet = require('helmet');
+
+const app = express();
+const PORT = 3000;
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../static')));
+
+app.listen(PORT, () => console.log('Listening to server on PORT:', PORT));
