170187bc89a6f223035497ed3f65050c8e7f3601
Kevin Jacobs <frevin8r@gmail.com>
2018-05-27 15:58:13 -0700

    added input and click events for app.jsx

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
+import { translateSentence } from '../../helpers/pigLatinTranslator';
 
 export default class App extends React.Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      term: '',
+      conversions: []
+    };
+    this.handleInput = this.handleInput.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  //on type update term
+  handleInput(e) {
+    console.log(e.target.value);
+    this.setState({
+      term: e.target.value
+    });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({
+      conversions: [...this.state.conversions, translateSentence(this.state.term)]
+    }, () => {
+      this.setState({
+        term: ''
+      }, () => {
+        console.log('current state term',this.state.term);
+        console.log('current state conversions',this.state.conversions);
+      })
+    });
+
   }
 
   render() {
     return(
       <div>
-        The app has rendered
+        <span>Enter a sentence to be translated!</span>
+        <input type="text" onKeyUp={(e) => this.handleInput(e)}/>
+        <button onClick={this.handleClick}>Convert</button>
       </div>
     )
   }
